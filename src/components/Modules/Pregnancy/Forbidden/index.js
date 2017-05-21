5de8d6f215b86f31e63f8986444f849501d70098
LONG <v5zhu@qq.com>
2017-05-21 15:49:02 +0800

    辐射相关页面初始化构建

--- src/components/Modules/Pregnancy/Forbidden/index.js
+++ src/components/Modules/Pregnancy/Forbidden/index.js
@@ -1,5 +1,14 @@
 module.exports = {
-    Echarts: require('./Echarts/Echarts.vue'),
-    Form: require('./Form/Form.vue'),
-    List: require('./List/List.vue'),
+  Echarts: require('./Echarts/Echarts.vue'),
+  Form: require('./Form/Form.vue'),
+  Radiation: require('./Radiation/Radiation.vue'),
+  Diet: require('./Diet/Diet.vue'),
+  Fruit: require('./Fruit/Fruit.vue'),
+  Life: require('./Life/Life.vue'),
+  Medicine: require('./Medicine/Medicine.vue'),
+  Clothing: require('./Clothing/Clothing.vue'),
+  Sexual: require('./Sexual/Sexual.vue'),
+  Sport: require('./Sport/Sport.vue'),
+  Lyingin: require('./Lyingin/Lyingin.vue'),
+  Work: require('./Work/Work.vue'),
 };
