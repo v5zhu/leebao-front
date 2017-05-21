d756bcd26791c171f3ffaf953c5a9d0e68c52213
LONG <v5zhu@qq.com>
2017-05-21 13:51:19 +0800

    完成辐射统计

--- src/components/Modules/Pregnancy/Forbidden/List/List.vue
+++ src/components/Modules/Pregnancy/Forbidden/List/List.vue
@@ -66,6 +66,12 @@
         :sortable="fields.author.info.sortable">
       </el-table-column>
     </el-table>
+    <ul type="circle" style="margin-left: 20px">
+      <li>挑选正规厂家生产的合格家电产品</li>
+      <li>不要把家用电器摆的过于集中</li>
+      <li>缩短使用电器的时间</li>
+      <li>有条件的孕妈咪可使用产品质量合格,有相关检测证明的防辐射服装、电视防辐射屏、防辐射窗帘等</li>
+    </ul>
   </div>
 </template>
 
@@ -81,7 +87,7 @@
           harm: '怀孕前3个月胎儿处于器官形成关键期,X光可能导致细胞组织突变从而流产或先天畸形',
           defense: '医院X光照射腹部遮盖,尽量避免此类检查',
           date: '2017-05-20',
-          author: 'Z.H',
+          author: 'vvboot',
         }, {
           name: '电热毯',
           star: '5',
@@ -89,7 +95,7 @@
           harm: '容易导致胎儿细胞分裂,发生异常改变,骨骼对电磁辐射最为敏感,影响胎儿大脑、神经、骨骼和心脏',
           defense: '禁止使用电热毯',
           date: '2017-05-20',
-          author: 'Z.H',
+          author: 'vvboot',
         }, {
           name: '电吹风',
           star: '4',
@@ -97,7 +103,7 @@
           harm: '头晕、疲乏无力、记忆力减退、食欲减退、失眠、健忘等亚健康状态',
           defense: '尽量不要用,使用干发毛巾或干毛帽代替',
           date: '2017-05-20',
-          author: 'Z.H',
+          author: 'vvboot',
         }, {
           name: '微波炉',
           star: '4',
@@ -105,7 +111,47 @@
           harm: '',
           defense: '不要放卧室,人不要站旁边,不用时拔掉电源,停止运行时再过去处理食品,准妈妈勿用微波炉',
           date: '2017-05-20',
-          author: 'Z.H',
+          author: 'vvboot',
+        }, {
+          name: '电脑',
+          star: '3',
+          feature: '开机时周边存在电磁辐射,包括X射线、紫外线、可见光、红外线和特高频、高频、中频及极低频电磁场;背面辐射最强,两侧其次,正面最弱',
+          harm: '在细胞膜水平上干扰细胞的代谢和增殖,从而影响胚胎的发育,其次电脑辐射是导致宝宝听力残疾的头号危险因素',
+          defense: '保持安全距离,穿防辐射服,控制时间',
+          date: '2017-05-20',
+          author: 'vvboot',
+        }, {
+          name: '手机',
+          star: '2',
+          feature: '工作状态时向基站传送无线电波,通话接通时比通话中辐射高20倍,信号不好电量低时,辐射更高',
+          harm: '或多或少被人体吸收,从而改变人体组织',
+          defense: '专用耳机、麦克风接听电话,减少通话时间,电话未接通时避免靠近耳部',
+          date: '2017-05-20',
+          author: 'vvboot',
+        }, {
+          name: '电视机',
+          star: '2',
+          feature: '传统电视电子束打在荧光粉上的瞬间会产生电磁辐射,产生X射线,液晶电视相对辐射小很多',
+          harm: '...',
+          defense: '保持2米以上距离,连续不能超过2小时,看完电视洗脸',
+          date: '2017-05-20',
+          author: 'vvboot',
+        }, {
+          name: '复印机/打印机',
+          star: '1',
+          feature: '有电流就有电磁波即存在辐射,打印机的电子线圈和风扇部位辐射最大',
+          harm: '...',
+          defense: '办公一族保持30厘米以上或穿防辐射服',
+          date: '2017-05-20',
+          author: 'vvboot',
+        }, {
+          name: '安检',
+          star: '1',
+          feature: '主要通过电磁场的辐射对金属物探测,电磁辐射小',
+          harm: '...',
+          defense: '不超过6分钟,减少乘飞机,准妈妈尽快通过安检',
+          date: '2017-05-20',
+          author: 'vvboot',
         }],
         selectData: {
           name: '',
@@ -121,31 +167,31 @@
             },
             filter: {},
             style: {
-              width: '146',
+              width: '130',
               align: 'center'
             }
           },
           star: {
             info: {
               prop: 'star',
-              label: '指数',
+              label: '辐射指数',
               sortable: true
             },
             filter: {
               list: [{
-                text: '*',
+                text: '★',
                 value: 1
               }, {
-                text: '**',
+                text: '★★',
                 value: 2
               }, {
-                text: '***',
+                text: '★★★',
                 value: 3
               }, {
-                text: '****',
+                text: '★★★★',
                 value: 4
               }, {
-                text: '*****',
+                text: '★★★★★',
                 value: 5
               }],
               multiple: false
@@ -224,15 +270,15 @@
        */
       formatterStar(item) {
         if (item.star == 1) {
-          return '*';
+          return '★';
         } else if (item.star == 2) {
-          return '**';
+          return '★★';
         } else if (item.star == 3) {
-          return '***';
+          return '★★★';
         } else if (item.star == 4) {
-          return '****';
+          return '★★★★';
         } else if (item.star == 5) {
-          return '*****';
+          return '★★★★★';
         }
       },
       filterStar(value, item) {
@@ -245,5 +291,7 @@
   }
 </script>
 <style scoped>
-
+    *{
+      font-size: 12px;
+    }
 </style>
